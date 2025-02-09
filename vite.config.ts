13ba28b8492263ed52886645f38f2b8dbb135b38
Alban Thaumur <thaumur.alban@gmail.com>
2025-02-09 17:02:17 +0100

    style: add tailwindcss and shadcn and configure them

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,16 @@ import { getDirname } from '@adonisjs/core/helpers'
 import inertia from '@adonisjs/inertia/client'
 import react from '@vitejs/plugin-react'
 import adonisjs from '@adonisjs/vite/client'
+import tailwindcss from 'tailwindcss'
+import autoPrefixer from 'autoprefixer'
 
 export default defineConfig({
   plugins: [inertia({ ssr: { enabled: true, entrypoint: 'inertia/app/ssr.tsx' } }), react(), adonisjs({ entrypoints: ['inertia/app/app.tsx'], reload: ['resources/views/**/*.edge'] })],
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoPrefixer()],
+    },
+  },
 
   /**
    * Define aliases for importing modules from
