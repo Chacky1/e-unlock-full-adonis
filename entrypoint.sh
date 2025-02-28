163a820a2782f4b719ceeec54480bb01710c6561
Alban Thaumur <thaumur.alban@gmail.com>
2025-02-28 12:50:39 +0100

    fix: Fix automated migration run

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -e
 
-echo "Run migrations..."
-node ace migration:run
-
 if [ "$NODE_ENV" = "production" ]; then
   echo "Production server is being started..."
   exec node ace serve --watch=false
